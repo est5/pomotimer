1ebb68a03a961c1370ff33d5a85287e23ff11337
est5 <ghostrekon5500@ya.ru>
2022-09-30 12:44:49 +0300

    add timer and reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+
+const SECOND = 1;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(false);
+  const [reset, setReset] = useState(false);
+
+  let timeoutId: number = 0;
+
+  useEffect(() => {
+    if (reset) {
+      setCount(0);
+      return;
+    }
+    if (timer && !reset) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setCount(count + SECOND), 1000);
+    }
+  }, [timer, count, reset]);
+
+  const resetTimer = () => {
+    setReset(true);
+    setTimer(false);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <h1>Timer</h1>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setTimer(!timer);
+          setReset(false);
+        }}
+      >
+        {' '}
+        {!timer ? 'Start' : 'Stop'}{' '}
+      </button>
+      <button onClick={resetTimer}>Reset</button>
+    </>
+  );
 }
 
-export default App
+export default App;

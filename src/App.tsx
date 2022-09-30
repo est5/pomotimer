f6db64a3d41660313e64ac12f8e0f5c8207d0c57
est5 <ghostrekon5500@ya.ru>
2022-09-30 17:23:53 +0300

    add timer logic mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+enum Focus {
+  WORK,
+  CHILL,
+}
 
 function App() {
   const [count, setCount] = useState(0);
   const [timer, setTimer] = useState(false);
   const [reset, setReset] = useState(false);
   const [result, setResult] = useState('0:00');
+  const [focusType, setFocusType] = useState(Focus.WORK);
+  const [minutes, setMinutes] = useState(0);
+  const [workTime, setWorkTime] = useState(40);
+  const [chillTime, setChillTime] = useState(10);
 
   let timeoutId: number = 0;
 
@@ -21,25 +30,58 @@ function App() {
   }, [timer, count, reset]);
 
   const resetTimer = () => {
+    setResult('00:00');
     setReset(true);
     setTimer(false);
   };
 
   useEffect(() => {
-    const minutes = Math.floor(count / 60);
+    setMinutes(Math.floor(count / 60));
     const seconds = count % 60;
 
     function padTo2Digits(num: number) {
       return num.toString().padStart(2, '0');
     }
 
-    setResult(`${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`);
-    console.log(result);
+    reset
+      ? setResult('00:00')
+      : setResult(`${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`);
   }, [count]);
+
+  useEffect(() => {
+    switch (focusType) {
+      case 0:
+        {
+          if (minutes == workTime) {
+            setFocusType(1);
+            resetTimer();
+          }
+        }
+        break;
+
+      case 1:
+        {
+          if (minutes == chillTime) {
+            setFocusType(0);
+            resetTimer();
+          }
+        }
+        break;
+    }
+  }, [count]);
+
+  const validateInput = (n: number) => {
+    if (n >= 60) {
+      return 60;
+    } else if (n <= 1) {
+      return 1;
+    }
+    return n;
+  };
 
   return (
     <>
-      <h1>Timer</h1>
+      <h1>{focusType == 0 ? 'Work' : 'Chill'}</h1>
       <p>{result}</p>
       <button
         onClick={() => {
@@ -47,10 +89,46 @@ function App() {
           setReset(false);
         }}
       >
-        {' '}
-        {!timer ? 'Start' : 'Stop'}{' '}
+        {!timer ? 'Start' : 'Stop'}
       </button>
       <button onClick={resetTimer}>Reset</button>
+      <button
+        onClick={() => {
+          resetTimer();
+          setFocusType(focusType == 0 ? 1 : 0);
+        }}
+      >
+        Skip to {focusType == 0 ? 'Chill' : 'Work'}
+      </button>
+
+      <div>
+        <label htmlFor="work">Work timer(min)</label>
+        <input
+          type="number"
+          name="work"
+          id="work"
+          max={60}
+          min={1}
+          value={workTime}
+          onChange={(e: React.FormEvent<HTMLInputElement>) =>
+            setWorkTime(validateInput(Number.parseInt(e.currentTarget.value)))
+          }
+        />
+      </div>
+      <div>
+        <label htmlFor="chill">Chill timer(min)</label>
+        <input
+          type="number"
+          name="chill"
+          id="chill"
+          max={60}
+          min={1}
+          value={chillTime}
+          onChange={(e: React.FormEvent<HTMLInputElement>) =>
+            setChillTime(validateInput(Number.parseInt(e.currentTarget.value)))
+          }
+        />
+      </div>
     </>
   );
 }
